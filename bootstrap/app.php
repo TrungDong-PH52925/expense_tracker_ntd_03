5495b551301a7c83a620f7844a30a57b37bb05b4
TrungDong-PH52925 <dongntph52925@gmail.com>
2026-03-30 11:12:52 +0700

    test WIP changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,10 +13,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // $middleware->api( prepend: [
-        //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class
-        // ]);
+        $middleware->api( prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+        $middleware->validateCsrfTokens(['api/*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Throwable $e, Request $req){
+                if ($req->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ], 500);
+            }
+        });
+
+
     })->create();
